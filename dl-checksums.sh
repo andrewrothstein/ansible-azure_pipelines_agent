be980abe112ae45b87b8fec76f5ccab7dff207fd
Andrew Rothstein <andrew.rothstein@gmail.com>
2020-09-14 22:28:41 -0400

    initial commit

---
+++ dl-checksums.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env sh
+set -e
+DIR=~/Downloads
+MIRROR=https://vstsagentpackage.azureedge.net/agent
+
+dl() {
+    local ver=$1
+    local os=$2
+    local arch=$3
+    local archive_type=${4:-tar.gz}
+
+    local platform="${os}-${arch}"
+
+    local file="vsts-agent-${platform}-${ver}.${archive_type}"
+    local lfile=$DIR/$file
+    local url="$MIRROR/$ver/$file"
+
+    if [ ! -e $lfile ];
+    then
+        wget -q -O $lfile $url
+    fi
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
+}
+
+dl_ver() {
+    local ver=$1
+
+    printf "  '%s':\n" $ver
+
+    dl $ver win x64 zip
+    dl $ver win x86 zip
+    dl $ver osx x64
+    dl $ver linux x64
+    dl $ver linux arm
+    dl $ver linux arm64
+    dl $ver rhel.6 x64
+}
+
+dl_ver ${1:-2.174.2}
